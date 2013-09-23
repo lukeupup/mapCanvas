d64196950e64bf438efb763487b3e65fc4015f4b
Luke <lukeupup@gmail.com>
2013-09-23 16:48:16 +0800

    rewrite the Area class

--- src/jquery.mapCanvas.js
+++ src/jquery.mapCanvas.js
@@ -1,63 +1,202 @@
 (function(window, $, Raphael, undefined){
 
-  var District = function(){
-
-  };
-
-  var paintMap = function(data, canvas, option){
-    var areaAttr = {
+  var Area = function(pathData, canvas, option){
+    var me = this,
+        cmdStr = '',
+        box,
+        pathAttr = {
           'fill'            : option.fillColor,
           'stroke'          : option.strokeColor,
           'stroke-width'    : option.strokeWidth,
-          'stroke-linejoin' : option.strokeLinejoin
+          'stroke-linejoin' : option.strokeLinejoin,
+          'cursor'          : option.cursor
         },
         textAttr = {
           'fill'            : option.textColor,
           'font-size'       : option.fontSize,
-          'font-family'     : option.fontFamily
+          'font-family'     : option.fontFamily,
+          'cursor'          : option.cursor
         },
-        areas = [];
+        zoom = option.zoom,
+        syncMouseEvents = ['mouseenter', 'mouseleave', 'mouseover', 'mouseout',
+          'mousemove', 'click'],
+        $container = $(canvas.canvas).parent();
 
-    $.each(data.paths, function(i, path){
-      var cmdStr = '',
-          area,
-          box,
-          text;
-      $.each(path.cmd, function(i, cmd){
-        var paramStr = '';
-        paramStr += $.map(cmd.param, function(p, i){
-          return p * option.zoom;
-        }).join(',');
+    me._option = option;
+    me._forceColored = false;
+    me._eventHandlers = {};
+    me._rawEventHandlers = {};
 
-        paramStr = paramStr.replace(',-', '-');
-        cmdStr += cmd.method;
-        cmdStr += paramStr;
-      });
+    me.name = pathData.name;
+    me.path = null;
+    me.text = null;
 
-      area = canvas.path(cmdStr).attr(areaAttr);
-      box = area.getBBox();
+    $.each(pathData.cmd, function(i, cmd){
+      var paramStr = '';
+      paramStr += $.map(cmd.param, function(p, i){
+        return p * option.zoom;
+      }).join(',');
+      paramStr = paramStr.replace(',-', '-');
+      cmdStr += cmd.method;
+      cmdStr += paramStr;
+    });
 
-      text = canvas.text(
-        box.x + (box.width / 2) + (path.offset ? path.offset.x * option.zoom : 0),
-        box.y + (box.height / 2) + (path.offset ? path.offset.y * option.zoom : 0),
-        path.name
-      ).attr(textAttr);
+    me.path = canvas.path(cmdStr).attr(pathAttr);
 
-      areas.push({
-        name: path.name,
-        area: area,
-        text: text
+    box = me.path.getBBox();
+
+    me.text = canvas.text(
+      box.x + (box.width / 2) + (pathData.offset ? pathData.offset.x * zoom : 0),
+      box.y + (box.height / 2) + (pathData.offset ? pathData.offset.y * zoom : 0),
+      me.name
+    ).attr(textAttr);
+
+    $.each(syncMouseEvents, function(i, eventName){
+      $(me.text.node)[eventName](function(e){
+        var canvasOffset = $container.offset(),
+            e = $.Event(eventName, {
+              pageX  : e.pageX,
+              pageY  : e.pageY,
+            }),
+            coordinate = {
+              x: e.pageX - canvasOffset.left,
+              y: e.pageY - canvasOffset.top
+            };
+
+
+        /*
+        NOTE: Raphael.isPointInsidePath() have some bugs so that we can't use
+        it to determine if we should trigger mouse events of me.path.  
+        So before the bug is fixed, mouseenter (as well as mouseleave, etc.)
+        will be also triggered when we hover the text, even if the text is inside
+        the path.
+        If this bug is fixed, we can use the following code to avoid this issue.
+        
+        See bug detail at:
+        https://github.com/DmitryBaranovskiy/raphael/issues/539
+
+        // don't trigger mouseenter / mouseleave / mouseover / mouseout when the text is inside the path.
+        if (!Raphael.isPointInsidePath(cmdStr, coordinate.x, coordinate.y) ||
+          (eventName === 'click' || eventName === 'mousemove')
+        ) {
+          $(me.path.node).trigger(e);
+        }
+        */
+        $(me.path.node).trigger(e);
+
       });
 
     });
 
-    $.each(areas, function(i, area){
-      area.text.toFront();
+    me.on('mouseenter', function(){
+      if (!me._forceColored) {
+        me.path.animate({'fill': option.hoverFillColor}, 100);
+        me.text.animate({'fill': option.hoverTextColor}, 100);
+      }
+    }).on('mouseleave', function(){
+      if (!me._forceColored) {
+        me.path.animate({'fill': option.fillColor}, 100);
+        me.text.animate({'fill': option.textColor}, 100);
+      }
     });
-
-    return areas;
 
   };
+
+  Area.prototype.on = function(eventType, rawHandler) {
+    var me = this,
+        handler = function(){
+          rawHandler.apply(me, arguments);
+        };
+
+    if (me._eventHandlers[eventType]) {
+      me._eventHandlers[eventType].push(handler);
+      me._rawEventHandlers[eventType].push(rawHandler);
+    }
+    else {
+      me._eventHandlers[eventType] = [handler];
+      me._rawEventHandlers[eventType] = [rawHandler];
+    }
+
+    $(me.path.node).on(eventType, handler);
+    return me;
+  };
+
+  Area.prototype.off = function(eventType, rawHandler) {
+    var me = this,
+        handler,
+        i;
+
+    if (!me._rawEventHandlers[eventType]) {
+      return me;
+    }
+    for (i = 0; i < me._rawEventHandlers[eventType].length; i++) {
+      if (me._rawEventHandlers[eventType][i] === rawHandler) {
+        me._rawEventHandlers[eventType].splice(i, 1)[0];
+        handler = me._eventHandlers[eventType].splice(i, 1)[0];
+        break;
+      }
+    }
+    if (handler) {
+      $(me.path.node).off(eventType, handler);
+    }
+    return me;
+  };
+
+  Area.data = function(){
+    var me = this,
+        $path = $(me.path.node);
+    return $path.data.apply($path, arguments);
+  };
+  Area.removeData = function(){
+    var me = this,
+        $path = $(me.path.node);
+    return $path.removeData.apply($path, arguments);
+  }
+
+  Area.prototype.setFillColor = function(color) {
+    var me = this;
+    if (!color) {
+      me.path.animate({'fill': me._option.fillColor},  100);
+      me._forceColored = false;
+    }
+    else {
+      me.path.animate({'fill': color}, 100);
+      me._forceColored = true;
+    }
+    return me;
+  };
+
+  var paintMap = function(data, canvas, option){
+    var areas = [];
+
+    $.each(data.paths, function(i, path){
+      areas.push(new Area(path, canvas, option));
+    });
+    return areas;
+  };
+
+
+  var bindBubble = function(areas, option){
+    var $bubble = $('<div class="J_MapBubble"></div>').appendTo('body')
+      .hide().css(option.bubbleStyle);
+
+    $.each(areas, function(i, area){
+      area.on('mouseenter', function(e){
+        $bubble.html(option.bubble(area.name)).css({
+          top:  (e.pageY) + option.bubbleOffset.y,
+          left: (e.pageX) + option.bubbleOffset.x
+        }).show();
+      }).on('mousemove', function(e){
+        $bubble.css({
+          top:  (e.pageY) + option.bubbleOffset.y,
+          left: (e.pageX) + option.bubbleOffset.x
+        });
+      }).on('mouseleave', function(e){
+        $bubble.hide().empty();
+      });
+    });
+
+  }
 
 
   $.fn.mapCanvas = function(data, option){
@@ -76,6 +215,7 @@
       hoverTextColor: '#555',
       fontSize:       '12px',
       fontFamily:     'arial, sans-serif',
+      cursor:         'pointer',
       bubble: function(name){
         return name;
       },
@@ -85,12 +225,13 @@
       },
       bubbleStyle: {
         background:   '#333',
+        background:   '#333',
+        background:   '#333',
         color:        '#FFF',
         borderRadius: '3px',
         padding:      '5px 10px'
       },
 
-      onClickArea: $.noop,
       onMapInit: $.noop
     }, option);
 
@@ -114,58 +255,11 @@
     $this.each(function(i, elem){
       var $elem = $(elem).width(option.width).height(option.height).empty(),
           canvas = Raphael(elem, option.width, option.height),
-          map = paintMap(data, canvas, option),
-          $bubble;
+          map = paintMap(data, canvas, option);
 
       if (option.bubble) {
-        $bubble = $('<div class="J_MapBubble"></div>').appendTo('body')
-          .hide().css(option.bubbleStyle);
+        bindBubble(map, option);
       }
-
-      $.each(map, function(i, area){
-        var $node = $(area.area.node),
-            $text = $(area.text.node);
-
-        $node.mouseenter(function(e, x, y){
-          if (option.bubble) {
-            $bubble.html(option.bubble(area.name)).css({
-              top:  (y || e.pageY) + option.bubbleOffset.y,
-              left: (x || e.pageX) + option.bubbleOffset.x
-            }).show();
-          }
-          area.area.animate({fill: option.hoverFillColor}, 100);
-          area.text.animate({fill: option.hoverTextColor}, 100);
-        }).mousemove(function(e, x, y){
-          if (option.bubble) {
-            $bubble.css({
-              top:  (y || e.pageY) + option.bubbleOffset.y,
-              left: (x || e.pageX) + option.bubbleOffset.x
-            });
-          }
-
-        }).mouseleave(function(e){
-          if (option.bubble) {
-            $bubble.hide().empty();
-            area.area.animate({fill: option.fillColor}, 100);
-            area.text.animate({fill: option.textColor}, 100);
-          }
-        }).click(function(e){
-          if (option.onClickArea) {
-            option.onClickArea(area);
-          }
-        });
-
-        $text.mouseenter(function(e){
-          $node.trigger('mouseenter', [e.pageX, e.pageY]);
-        }).mousemove(function(e){
-          $node.trigger('mousemove', [e.pageX, e.pageY]);
-        }).mouseleave(function(e){
-          $node.trigger('mouseleave');
-        }).click(function(){
-          $node.trigger('click');
-        });
-      });
-      
 
       if (option.onMapInit) {
         option.onMapInit();
